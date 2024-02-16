c085a62d208b311bd9d9a3fc477a823e9a741b44
Egor Shievanov <shievanov@bk.ru>
2024-02-16 15:44:56 +0300

    установил зависимости

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # установленные пакеты
-    'django_extensions',
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
